6281c93ead9dab73629d1c1dc2e24391d3dddc04
czm <chenzhenmin@dossen.com>
2020-01-10 18:26:14 +0800

    modify pcIndex page

--- src/page/index/pcIndex.vue
+++ src/page/index/pcIndex.vue
@@ -1,13 +1,29 @@
 <template>
     <div id="pcIndex">
-      <div></div>
-      <div>
-        <div>
-          <ul>
-            <li></li>
-          </ul>
+      <div class="header">
+        <span class="title">My VueCli</span>
+        <ul>
+          <li class="active">示例</li>
+          <li @click="goGitHub()">github</li>
+        </ul>
+      </div>
+      <div class="nav">
+        <ul v-for="item in content" :key="item.id">
+          <li>
+            <span class="title" @click="clickElement($event)">{{item.name}}<i></i></span>
+            <dl>
+              <dd v-for="item in item.construction" :key="item.id">{{item.name}}</dd>
+            </dl>
+          </li>
+        </ul>
+      </div>
+      <div class="body">
+        <ul>
+          <li class="active">预览</li>
+        </ul>
+        <div class="body-content">
+          <iframe src="/pcPage/cityArea"></iframe>
         </div>
-        <div></div>
       </div>
     </div>
 </template>
@@ -17,13 +33,37 @@ export default {
   name: 'pcIndex',
   data() {
     return {
-      components: [{
-        name: 'CityArea 城市选择',
-        url: '/pcPage/cityArea'
-      },{
-        name: 'slideShow 轮播图',
-        url: '/pcPage/slideShow'
+      content: [{
+        name: '组件',
+        construction:[
+        {
+          name: 'CityArea 城市选择',
+          url: '/pcPage/cityArea'
+        },
+        {
+          name: 'slideShow 轮播图',
+          url: '/pcPage/slideShow'
+        }]
       }]
+    }
+  },
+  methods: {
+    /**
+     * 跳转github
+     */
+    goGitHub: function(){
+      window.location.href = "https://github.com/czm1290433700/my_vue_cli";
+    },
+    /**
+     * 点击导航元素
+     */
+    clickElement: function(item){
+      let dlElement = item.currentTarget.nextElementSibling;
+      if(dlElement.style.display == "none" || dlElement.style.display == ""){
+        dlElement.style.display = "block";
+      }else{
+        dlElement.style.display = "none"
+      }
     }
   }
 }
