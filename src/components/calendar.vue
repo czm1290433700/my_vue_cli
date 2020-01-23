761954dbe7cdb6ee8bc31b21a637ac7fb5e1b19e
czm <1290433700@qq.com>
2020-01-23 23:04:12 +0800

    modify calendar component input style and add focus method

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="calendar">
-        <input1></input1>
-        <div class="calendarWidget">
+        <input type="text" class="timeInput" placeholder="请选择时间" @focus="calendarShow = true"/>
+        <div class="calendarWidget" v-show="calendarShow">
             <div class="title">
                 <div class="left" @click="yearBack()">
                     <i></i>
@@ -32,22 +32,22 @@
             </div>
             <div class="calendarBody">
                 <ul ref="week1">
-                    <li v-for="item in week1" :key="item.id">{{item}}</li>
+                    <li v-for="item in week1" :key="item.id" @click="chooseDate($event)">{{item}}</li>
                 </ul>
                 <ul ref="week2">
-                    <li v-for="item in week2" :key="item.id">{{item}}</li>
+                    <li v-for="item in week2" :key="item.id" @click="chooseDate($event)">{{item}}</li>
                 </ul>
                 <ul ref="week3">
-                    <li v-for="item in week3" :key="item.id">{{item}}</li>
+                    <li v-for="item in week3" :key="item.id" @click="chooseDate($event)">{{item}}</li>
                 </ul>
                 <ul ref="week4">
-                    <li v-for="item in week4" :key="item.id">{{item}}</li>
+                    <li v-for="item in week4" :key="item.id" @click="chooseDate($event)">{{item}}</li>
                 </ul>
                 <ul ref="week5">
-                    <li v-for="item in week5" :key="item.id">{{item}}</li>
+                    <li v-for="item in week5" :key="item.id" @click="chooseDate($event)">{{item}}</li>
                 </ul>
                 <ul ref="week6">
-                    <li v-for="item in week6" :key="item.id">{{item}}</li>
+                    <li v-for="item in week6" :key="item.id" @click="chooseDate($event)">{{item}}</li>
                 </ul>
             </div>
         </div>
@@ -60,6 +60,7 @@ export default {
     name: 'calendar',
     data() {
         return {
+            calendarShow: false,
             year: 0,
             month: 0,
             lastMonthDays: 0, // 上月显示天数
