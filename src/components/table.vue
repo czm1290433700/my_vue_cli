2b519e7e0cdcb0b3ba99cbe52f218adbd719b316
czm <1290433700@qq.com>
2020-01-29 20:25:47 +0800

    modify table component arrow color bug and sort bug

--- src/components/table.vue
+++ src/components/table.vue
@@ -4,8 +4,8 @@
             <th v-for="(item,index) in tableHead" :key="index">
                 <div class="tableHead">
                     {{item}}
-                    <div class="increaseSort" @click="increaseSort(index)" ref="topArrow"></div>
-                    <div class="decreaseSort" @click="decreaseSort(index)" ref="bottomArrow"></div>
+                    <div class="increaseSort" @click="increaseSort(index, $event)" ref="topArrow"></div>
+                    <div class="decreaseSort" @click="decreaseSort(index, $event)" ref="bottomArrow"></div>
                 </div>
             </th>
         </tr>
@@ -30,30 +30,43 @@ export default {
         /** 
          * 增序排序
          */
-        increaseSort: function(index){
+        increaseSort: function(index, item){
             this.tableData.sort(function(a, b){
-                if(tools.isChinese(a) && tools.isChinese(b)){
+                if(tools.isChinese(a[index]) && tools.isChinese(b[index])){
                     return b[index].localeCompare(a[index]);
                 }else{
                     return a[index] > b[index];
                 }
             });
             this.tableData = this.tableData.concat();
-            this.$refs.topArrow.style.borderBottom = '5px solid #000';
+            this.initArrow();
+            item.currentTarget.style.borderBottom = '5px solid #000';
         },
         /** 
          * 降序排序
          */
-        decreaseSort: function(index){
+        decreaseSort: function(index, item){
             this.tableData.sort(function(a, b){
-                if(tools.isChinese(a) && tools.isChinese(b)){
+                if(tools.isChinese(a[index]) && tools.isChinese(b[index])){
                     return a[index].localeCompare(b[index]);    // 按照汉语拼音顺序
                 }else{
                     return a[index] < b[index];
                 }
             });
             this.tableData = this.tableData.concat();
-            this.$refs.bottomArrow.style.borderTop = '5px solid #000';
+            this.initArrow();
+            item.currentTarget.style.borderTop = '5px solid #000';
+        },
+        /**
+         * 箭头样式初始化
+         */
+        initArrow: function(){
+            for(let i = 0; i < this.$refs.topArrow.length; i++){
+                this.$refs.topArrow[i].style.borderBottom = '5px solid #B2B2B2';
+            }
+            for(let i = 0; i < this.$refs.bottomArrow.length; i++){
+                this.$refs.bottomArrow[i].style.borderTop = '5px solid #B2B2B2';
+            }
         }
     }
 }
