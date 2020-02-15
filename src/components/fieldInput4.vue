d338a61a07b16f51e5e3262966131b5653d910b9
czm <1290433700@qq.com>
2020-02-15 10:32:06 +0800

    modify fieldInput4 component

--- src/components/fieldInput4.vue
+++ src/components/fieldInput4.vue
@@ -2,13 +2,27 @@
     <div id="fieldInput4">
         <div class="title">验证码</div>
         <input type="text" placeholder="输入验证码">
-        <button type="button">获取验证码</button>
+        <button type="button" ref="btn" @touchstart="touchstart()" @touchend="touchend()">获取验证码</button>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'fieldInput4'
+    name: 'fieldInput4',
+    methods: {
+        /**
+         * 按钮按下事件
+         */
+        touchstart: function(){
+            this.$refs.btn.style.backgroundColor = "rgb(7, 167, 84)";
+        },
+        /**
+         * 按钮松开事件
+         */
+        touchend: function(){
+            this.$refs.btn.style.backgroundColor = "#07C160";
+        }
+    }
 }
 </script>
 
