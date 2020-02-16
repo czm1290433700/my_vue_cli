a215bbb42215cc9ebe59550d9c01b628f7873737
czm <1290433700@qq.com>
2020-02-16 20:14:19 +0800

    add search1 component

--- src/components/search1.vue
+++ src/components/search1.vue
@@ -1,10 +1,23 @@
 <template>
-    
+    <div id="search1">
+        <div class="searchArea">
+            <input type="text" ref="searchInput" placeholder="请输入关键词">
+            <img src="../assets/images/icon-clear.png" @click="clearInput()">
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'search1'
+    name: 'search1',
+    methods: {
+        /**
+         * 清空文本框
+         */
+        clearInput: function(){
+            this.$refs.searchInput.value = '';
+        }
+    }
 }
 </script>
 
