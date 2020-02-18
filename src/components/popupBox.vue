5cf0b938f5bb70e6725f12f77c06f45fca874c15
czm <1290433700@qq.com>
2020-02-18 17:28:51 +0800

    add popupBox component

--- src/components/popupBox.vue
+++ src/components/popupBox.vue
@@ -1,5 +1,6 @@
 <template>
-    <div id="popupBox" v-show="showPopup">
+    <div id="popupBox" ref="popupBox">
+        <span class="closeBtn" @click="hide()">X</span>
         <slot name="popupContent"></slot>
     </div>
 </template>
@@ -9,21 +10,46 @@ export default {
     name: 'popupBox',
     data() {
         return {
-            showPopup: false,
+            timer: new Function(),
+            offsetBottom: 0
         }
+    },
+    mounted () {
+        /**初始化弹出层隐藏 */
+        this.$refs.popupBox.style.bottom = this.$refs.popupBox.offsetHeight / -37.5 + 'rem';
+        this.offsetBottom = this.$refs.popupBox.offsetHeight / -37.5;
     },
     methods: {
         /**
          * 弹出层显示
          */
         show: function(){
-            this.showPopup = true;
+            var _this = this;
+            clearInterval(this.timer);
+            this.timer = setInterval(function(){
+                if(_this.offsetBottom >= 0){
+                    clearInterval(_this.timer);
+                }else{
+                    _this.offsetBottom = _this.offsetBottom + 0.5 > 0 ? 0 : _this.offsetBottom + 0.5;
+                    _this.$refs.popupBox.style.bottom = _this.offsetBottom + 'rem';
+                }
+            }, 20)
         },
         /**
          * 弹出层隐藏
          */
         hide: function(){
-            this.showPopup = false;
+            var _this = this,
+                height = _this.$refs.popupBox.offsetHeight / -37.5;
+            clearInterval(this.timer);
+            this.timer = setInterval(function(){
+                if(_this.offsetBottom <= height){
+                    clearInterval(_this.timer);
+                }else{
+                    _this.offsetBottom = _this.offsetBottom - 0.5 < height ? height : _this.offsetBottom - 0.5;
+                    _this.$refs.popupBox.style.bottom = _this.offsetBottom + 'rem';
+                }
+            }, 20)
         }
     }
 }
