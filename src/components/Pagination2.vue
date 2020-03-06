d54847dfdc1c39addf6cae10472b6399460f9cff
czm <1290433700@qq.com>
2020-03-06 19:53:22 +0800

    add pagination2 component

--- src/components/Pagination2.vue
+++ src/components/Pagination2.vue
@@ -1,10 +1,36 @@
 <template>
-    
+    <div id="pagination">
+        <ul>
+            <li :class="currentPage == 0 ? 'pageBtn noActiveBtn' : 'pageBtn'" @click="movePage(-1)">上一页</li>
+            <li class="pages">{{currentPage + 1}}/{{pages}}</li>
+            <li :class="currentPage == pages - 1 ? 'pageBtn noActiveBtn' : 'pageBtn'" @click="movePage(-2)">下一页</li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'Pagination2'
+    name: 'Pagination2',
+    props: ['pages'],
+    data () {
+        return {
+            currentPage: 0
+        }
+    },
+    methods: {
+        /**
+         * 修改页数
+         */
+        movePage: function(index){
+            if(index == -1 && this.currentPage != 0){
+                this.currentPage--;
+            }else if(index == -2 && this.currentPage != this.$props.pages - 1){
+                this.currentPage++;
+            }else if(index >= 0){
+                this.currentPage = index;
+            }
+        }
+    }
 }
 </script>
 
